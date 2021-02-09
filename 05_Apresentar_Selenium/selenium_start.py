b4123b9d93eb19bc22238ef87863771900332f11
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-09 14:01:46 -0300

    arrumando comentarios do 05

--- 05_Apresentar_Selenium/selenium_start.py
+++ 05_Apresentar_Selenium/selenium_start.py
@@ -1,8 +1,8 @@
-# Primeiro importe o webdriver que é o módulo que provê implementações para diferentes browsers
+# Primeiro importe o webdriver que é o módulo que provê implementações para diferentes browsers.
 from selenium import webdriver
 
-# Criamos uma instância do navegador que vamos utiliza, passando o caminho do webdriver baixado
+# Crie uma instância do navegador que vamos utiliza, passando o caminho do webdriver baixado.
 chrome = webdriver.Chrome('chromedriver_win32/chromedriver.exe')
 
-# com a instância do webdriver podemos utilizar o método 'get' para abrir a página especificada
+# Com a instância do webdriver podemos utilizar o método 'get' para abrir a página especificada.
 chrome.get('https://www.python.org/')
