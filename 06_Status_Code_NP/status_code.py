65dd57584deb70523c61dc350e9452661d29f5c4
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-10 07:34:57 -0300

    exibir corpo html da página 07

--- 06_Status_Code_NP/status_code.py
+++ 06_Status_Code_NP/status_code.py
@@ -1,11 +1,18 @@
 # Instalar biblioteca
 # pip install requests
+# pip install beautifulsoup4
 
-# importando biblioteca
+# importando as bibliotecas
 import requests
+from bs4 import BeautifulSoup
 
 # Realiza uma requizição do tipo get na url indicada
-req_get = requests.get('https://www.google.com')
+req_get = requests.get('https://github.com')
 
-# Printando o resultado
-print(req_get.status_code)
+# Printando o status code da requisição
+print('Status Code:', req_get.status_code)
+
+# Verifica se o status code é igual a 200
+if req_get.status_code == 200:
+    soup = BeautifulSoup(req_get.content, 'html.parser')
+    print(soup.prettify())
