f4113ee874a6862990ad03d8872452fe4fe06fc3
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-10 08:32:18 -0300

    finalizando 06

--- 06_Corpo_Html_Pagina/corpo_html.py
+++ 06_Corpo_Html_Pagina/corpo_html.py
@@ -1,18 +1,20 @@
-# Instalar biblioteca
-# pip install requests
-# pip install beautifulsoup4
-
 # importando as bibliotecas
 import requests
 from bs4 import BeautifulSoup
 
-# Realiza uma requizição do tipo get na url indicada
-req_get = requests.get('https://github.com')
+# Realiza uma requisição do tipo get na url indicada
+req_get = requests.get('http://www.google.com/')
 
-# Printando o status code da requisição
-print('Status Code:', req_get.status_code)
+# Printando o status code da requisição, e pulando uma linha
+print('Status Code:', req_get.status_code, '\n')
 
 # Verifica se o status code é igual a 200
 if req_get.status_code == 200:
-    soup = BeautifulSoup(req_get.content, 'html.parser')
+    # Acessa o corpo da resposta, retornando o html da página em bytes
+    html_doc = req_get.content
+
+    # Transformamos o conteúdo html recebido em obj python através do BeautifulSoup, para analisarmos
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    # Com o 'prettify' podemos exibir o corpo html de forma organizada
     print(soup.prettify())
