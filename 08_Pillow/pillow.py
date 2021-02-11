50acde44f255f547e563be7540f0d8916d166a02
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-11 07:51:41 -0300

    finalizando 08 - tratamento de imagens com Pillow

--- 08_Pillow/pillow.py
+++ 08_Pillow/pillow.py
@@ -1,6 +1,17 @@
-import PIL
+# Importando módulo utilizado
 from PIL import Image
 
-img = Image.open('08.png')
+# Especificamos o arquivo, que esta na pasta raiz do projeto
+file = 'imagem.jpg'
 
-img.show('08.png')
+# Instanciamos o módulo Image.open() para carregar a imagem
+img = Image.open(file)
+
+# Rotacionando imagem em 45 graus
+img_rotate = img.rotate(45)
+
+# Exibindo imagem rotacionada usando um visualizador externo
+img_rotate.show(file)
+
+# Salvando imagem na pasta raiz do projeto
+img_rotate.save(f'new_{file}')
