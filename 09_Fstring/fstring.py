ab8d91436fcdcf6dda6bd22104fe24a4b0184147
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-12 07:55:44 -0300

    iniciando 09 fstring

---
+++ 09_Fstring/fstring.py
@@ -0,0 +1,11 @@
+variavel = 'Python'
+
+print(f'{variavel:.1}')
+
+print(f'{variavel:.3}')
+
+print(f'{variavel:.{len(variavel)}}')
+
+print(f'[{variavel:>10}]')
+print(f'[{variavel:<10}]')
+print(f'[{variavel:^10}]')
