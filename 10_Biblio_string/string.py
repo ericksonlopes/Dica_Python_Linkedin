2e8c0a54fd1382b7edcaefb3b6f0a8f511fb1ec0
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-16 07:58:46 -0300

    Dica 10 - Biblio String

---
+++ 10_Biblio_string/string.py
@@ -0,0 +1,30 @@
+# Importação da biblioteca
+import string
+
+# String contendo todas as letras maiúsculas ASCII
+print(string.ascii_uppercase)
+# ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# String contendo todas as letras ASCII minúsculas
+print(string.ascii_lowercase)
+# abcdefghijklmnopqrstuvwxyz
+
+# String contendo todas as letras ASCII
+print(string.ascii_letters)
+# abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+# String contendo todos os dígitos decimais ASCII
+print(string.digits)
+# 0123456789
+
+# String contendo todos os caracteres de pontuação ASCII
+print(string.punctuation)
+# !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+
+# uma string contendo todos os espaços em branco ASCII
+print(string.whitespace)
+# 
+
+# uma string contendo todos os caracteres ASCII considerados imprimíveis
+print(string.printable)
+# 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ \t\n\r\v\f'
