84f1085cb2a102be77a08df68e7b8b017c6ea469
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-18 08:56:40 -0300

    13

---
+++ 13_matplotly/matplotlib_run.py
@@ -0,0 +1,13 @@
+# importe o módulo necessário
+from matplotlib import pyplot as plt
+
+# prepare os dados
+x = [1, 2, 3, 4, 5]
+y = [13, 23, 15, 48, 10]
+
+# Plote os dados
+plt.plot(x, y)
+
+# Exibe os dados no grafico
+plt.show()
+
