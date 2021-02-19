77cc40c963401a44cf596a45c0610165cf20593c
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-19 08:08:40 -0300

    finalizando - 07

--- 07_Barra_de_Progresso_NP/progress_bar.py
+++ 07_Barra_de_Progresso_NP/progress_bar.py
@@ -1,9 +1,11 @@
-# importando instância
+# importe as bibliotecas
 from time import sleep
 from tqdm import tqdm
 
-for cont in tqdm(range(100), desc='Contador'):
+# coloque dentro da função tqdm o iteravel a ser utilizado
+for cont in tqdm(range(30), desc='Contador 1'):
+    # sleep para cada vez no loop ter 30s
     sleep(0.30)
 
-
-# Barra de Progresso com python
+# utilizando list comprehension
+[sleep(0.30) for var in tqdm(range(30), desc='Contador 2')]
