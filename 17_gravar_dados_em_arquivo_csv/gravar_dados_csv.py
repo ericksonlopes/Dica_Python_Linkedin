992ce8f5bf6b9f34ce699cca08c88af8371b2f40
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-02-26 06:57:26 -0300

    add: criando script 17

---
+++ 17_gravar_dados_em_arquivo_csv/gravar_dados_csv.py
@@ -0,0 +1,15 @@
+import csv
+
+# Abre/cria o arquivo no modo leitura
+arquivo = open('arquivo.csv', 'w', newline='')
+
+# informa para o csv qual arquivio vai ser escrito
+escrever = csv.writer(arquivo)
+
+# Escrevendo no arquivo
+escrever.writerow(['Erickson', '18', 'Engenheiro de dados'])
+escrever.writerow(['Paulo', '25', 'Cientista de dados'])
+escrever.writerow(['Jackson', '21', 'Full stack'])
+
+# quando finalizar as alterações, feche o arquivo
+arquivo.close()
