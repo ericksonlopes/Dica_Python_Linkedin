40749808f9029e839826dbd52206700e8058f7fc
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-01 20:53:25 -0300

    add: adicionando varios testes

---
+++ Arte_ASCII/arte_ascii.py
@@ -0,0 +1,8 @@
+# instalar biblioteca
+# pip install pyfiglet
+
+import pyfiglet
+
+data = pyfiglet.figlet_format('Python')
+
+print(data)
---
+++ Enumerate_apartirde/enumerate.py
@@ -0,0 +1,4 @@
+lista = ['primeiro-item', 'segundo-item', 'terceiro-item']
+
+for idade, nome in enumerate(lista, 70):
+    print(f'{nome:>13} ', idade)
