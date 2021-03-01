9c1e5cf26681188935e0b3a41070960fe5dfd9b8
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-01 07:43:04 -0300

    add: img e comentarios 19

--- 19_notificacoes/notificacoes.py
+++ 19_notificacoes/notificacoes.py
@@ -1,8 +1,12 @@
+# Importando biblioteca e módulo
 from win10toast import ToastNotifier
 
+# instânciando módulo em uma variavel
 toast = ToastNotifier()
 
+# Passando os parâmetros para exibição da notificação
 toast.show_toast(title="Notificação do LinkedIn",       # Titulo
                  msg="Você tem uma nova notificação!",  # Mensagem
                  duration=20,                           # Duração
                  icon_path="linkedin_icon.ico")         # caminho do icone
+
