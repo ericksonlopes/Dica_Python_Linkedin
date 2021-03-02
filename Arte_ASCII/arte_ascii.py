cf45bdbf3c447f3b7fde8b430235689f3e263eb4
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-01 21:09:51 -0300

    listando todos os estilos da arte_ascii.py

--- Arte_ASCII/arte_ascii.py
+++ Arte_ASCII/arte_ascii.py
@@ -3,6 +3,16 @@
 
 import pyfiglet
 
-data = pyfiglet.figlet_format('Python')
+word = 'Python'
 
-print(data)
+data_figlet = pyfiglet.figlet_format(word)
+
+data_font = pyfiglet.print_figlet(word, font='digital')
+
+# print(data_figlet)
+# print(data_font)
+# print()
+
+for item in pyfiglet.FigletFont.getFonts():
+    print(item)
+    print(pyfiglet.print_figlet(word, font=item), '\n')
