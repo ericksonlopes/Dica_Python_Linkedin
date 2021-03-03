8c6e96834c649a7e06e862870c4c79b69bbb2515
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-03 08:38:00 -0300

    finish: 20

---
+++ 20_Lorem_Ipsum_Text_Generator/lorem_generator.py
@@ -0,0 +1,17 @@
+# instalar biblioteca
+# pip install lorem-text
+
+from lorem_text import lorem
+
+# Gere um único parágrafo
+print(lorem.paragraph())
+#Saida: Sapiente accusamus aliquid quas, magnam illo quas culpa facere assumenda,
+# voluptatum vero dicta numquam in dolorum necessitatibus possimus obcaecati.
+
+# Para frases geradas aleatoriamente de texto lorem ipsum em que a
+# primeira palavra é maiúscula e a frase termina em um ponto ou ponto de interrogação:
+print(lorem.sentence())
+# Saida:
+# Cumque tempore aperiam, aliquid distinctio unde perferendis exercitationem nulla deserunt,
+# cum unde quisquam corporis dolore dignissimos architecto commodi sapiente expedita esse,
+# aliquam est sint ex alias cum magnam adipisci numquam quidem saepe dicta?
---
+++ map_folium/get_map.py
@@ -0,0 +1,5 @@
+import folium
+
+map = folium.Map(location=[-23.715357, -46.85055197])
+
+map.save('Location.html')
