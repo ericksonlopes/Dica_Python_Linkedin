a585a23686f22e588c691eb9dbf0cd691c38ad40
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-13 22:35:02 -0300

    add: comentario

--- abrir_imagem/open_img.py
+++ abrir_imagem/open_img.py
@@ -1,5 +1,10 @@
+# instalar biblioteca
+# pip install PIL
+
 from PIL import Image
 
+# Carregar imagen na função
 image = Image.open('imagem.png')
 
+# Abre o app para exibir a imagen
 image.show()
