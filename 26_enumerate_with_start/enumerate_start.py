d40bba4384170aae1e0deb3c108efb7a95758b0a
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-18 07:46:35 -0300

    26 code

---
+++ 26_enumerate_with_start/enumerate_start.py
@@ -0,0 +1,6 @@
+items = ['Python', 'C++', 'JavaScript', 'R', 'C#']
+
+lista_gerada = list(enumerate(items, start=1))
+
+print(lista_gerada)
+# [(1, 'Python'), (2, 'C++'), (3, 'JavaScript'), (4, 'R'), (5, 'C#')]
--- Enumerate_function/enumerate_sep.py
+++ Enumerate_function/enumerate_sep.py
@@ -2,6 +2,3 @@ items = ['Maça', 'Banana', 'Grapes']
 
 for index, value in enumerate(items, 1):
     print(index, value, sep='-')
-
-
-print(list(enumerate(items, start=1)))
