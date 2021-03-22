18ee1cb43a8783c1ee0481b6f11299f7692bc21a
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-22 07:26:33 -0300

    27

--- 27_add_in_list/add_list.py
+++ 27_add_in_list/add_list.py
@@ -9,8 +9,11 @@ lista = lista + [3, 4, 5]
 lista.append(6)
 # [1, 2, 3, 4, 5, 6]
 
+lista_2 = [7, 8, 9]
+lista.extend(lista_2)  # concatena as listas
+# [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-
-
+lista.insert(10, 10)  # (posição, valor)
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 print(lista)
