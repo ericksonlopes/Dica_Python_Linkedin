f8743ba3729fddab454a295651583b90c8758c58
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-24 08:28:09 -0300

    add: print lib

---
+++ print_lib/lib_print.py
@@ -0,0 +1,8 @@
+import os
+import socket
+
+print(os)
+# <module 'os' from 'C:\\Users\\ofcer\\AppData\\Local\\Programs\\Python\\Python38\\lib\\os.py'>
+
+print(socket)
+# <module 'socket' from 'C:\\Users\\ofcer\\AppData\\Local\\Programs\\Python\\Python38\\lib\\socket.py'>
