cec946da0ab99ba6c09caaed66dc6974d3b13ebc
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-26 07:49:56 -0300

    finalizando código 30

--- 30_arquivo_CSV_faker/arquivo_csv_faker.py
+++ 30_arquivo_CSV_faker/arquivo_csv_faker.py
@@ -8,5 +8,7 @@ with open('data.csv', 'w', encoding='utf-8') as arquivo:
     arquivo.write('nome,idade,email,telefone\n')
     # for para que o código seja repetido 20 vezes
     for item in range(20):
-        # gera e escreve os dados dentro do arquivo
-        arquivo.write(f'{fake.name()},{fake.random.randint(0, 100)},{fake.email()},{fake.phone_number()}\n')
+        # gera os dados com a biblioteca
+        linha = f'{fake.name()},{fake.random.randint(0, 100)},{fake.email()},{fake.phone_number()}\n'
+        # escreve os dados dentro do arquivo
+        arquivo.write(linha)
