35a37263bf205242cc752214786d5b1d55e486c2
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-28 07:36:50 -0300

    comentando código - 30

--- 30_arquivo_CSV_faker/arquivo_csv_faker.py
+++ 30_arquivo_CSV_faker/arquivo_csv_faker.py
@@ -1,7 +1,11 @@
 from faker import Faker
 
+# Instanciando classe utilizada e configurando para pt-br
 fake = Faker('pt_br')
 
+# abre/cria um arquivo
 with open('data.csv', 'w', encoding='utf-8') as arquivo:
+    # for para que o código seja repetido 20 vezes
     for item in range(20):
+        # gera e escreve os dados dentro do arquivo
         arquivo.writelines(f'{fake.name()},{fake.email()},{fake.phone_number()}\n')
