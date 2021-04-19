6933d5aa8eff0178de337abecb3a8f4ce4b27bc4
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-04-19 07:47:20 -0300

    finish 35

--- 35_convertendo_video_em_audio/convert_va.py
+++ 35_convertendo_video_em_audio/convert_va.py
@@ -3,9 +3,10 @@
 
 import moviepy.editor
 
-# Passe o local
+# Como parametro, passe a localização do video
 video = moviepy.editor.VideoFileClip('video.mp4')
 
 audio_video = video.audio
 
+# Como parametro, passe o local onde sera salvo o audio
 audio_video.write_audiofile('audio.mp3')
