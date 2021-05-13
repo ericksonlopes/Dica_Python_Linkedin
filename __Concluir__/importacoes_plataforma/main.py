d2fa71fbecd1cf3e47f28ceb5423d3e189c4fbb6
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-05-13 14:00:18 -0300

    informações sobre o computador

---
+++ __Concluir__/importacoes_plataforma/main.py
@@ -0,0 +1,19 @@
+# biblioteca padrão
+
+import platform
+
+# Retorna o sistema
+print(platform.system())
+
+# Retorna o tipo de máquina
+print(platform.machine())
+
+# Retorna o (verdadeiro) nome do processador
+print(platform.processor())
+
+# Retorna a liberação do sistema
+print(platform.release())
+
+# Contém informações sobre a arquitetura de bits
+# e o formato de ligação usado para o executável.
+print(platform.architecture())
