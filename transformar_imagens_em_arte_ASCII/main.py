77710727101bc2778140b4355f650230cd457a65
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-05-15 15:49:56 -0300

    arte ascii com imagens

--- transformar_imagens_em_arte_ASCII/main.py
+++ transformar_imagens_em_arte_ASCII/main.py
@@ -1,7 +1,7 @@
 from pywhatkit import image_to_ascii_art
 
-path_img = 'img.png'
+path_img = 'python.png'
 
-nome_arquivo_txt = 'arte.txt'
+nome_arquivo_txt = 'ascii_python.txt'
 
 print(image_to_ascii_art(imgpath=path_img, output_file=nome_arquivo_txt))
