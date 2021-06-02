65f139aa95dc9622e00e317424b23bb609d41a34
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-06-02 12:59:38 -0300

    randrange elemento

---
+++ 39_randrange_elemento_aleatorio/main.py
@@ -0,0 +1,8 @@
+from random import randrange
+
+print(randrange(10, 50, 5))
+# Ex: 35
+print(randrange(10, 50, 5))
+# Ex: 10
+print(randrange(10, 50, 5))
+# Ex: 40
--- randrange_elemento_aleatorio/main.py
+++
@@ -1,9 +0,0 @@
-# Selecione um elemento aleatório de uma faixa #Python:
-from random import randrange
-
-print(randrange(10, 50, 2))
-# 38
-print(randrange(10, 50, 5))
-# 15
-print(randrange(10, 50, 5))
-# 45
