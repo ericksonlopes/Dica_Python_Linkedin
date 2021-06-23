5a9a3f0dc9c1fe323b1d4a82df4ef725b582deeb
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-06-23 09:26:20 -0300

    '40'

--- 40_verifica_email/main.py
+++ 40_verifica_email/main.py
@@ -1,7 +1,8 @@
+# instalar biblioteca
+# pip install validate_email
+
 from validate_email import validate_email
 
 print(validate_email('exemple@gmail.com'))
 # True
 
-print(validate_email('exemple@.com'))
-# False
