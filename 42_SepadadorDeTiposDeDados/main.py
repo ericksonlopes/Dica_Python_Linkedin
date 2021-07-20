cf86a26de03102dea5bf89bd1ad67ee3d0f5b8dd
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-07-20 17:46:40 -0300

    finalizando 42 código

--- 42_SepadadorDeTiposDeDados/main.py
+++ 42_SepadadorDeTiposDeDados/main.py
@@ -1,15 +1,29 @@
-lista = [[1, 2, 3], 'Python', 1, 1.2, [4, 5], 2, 'C++']
-
-tipos = [type(item) for item in lista]
-
+lista = [[1, 2, 3], 'Python', 1, 1.2, [4, 5], 2, 'C++', 3.5]
 dicio = {}
 
-for tipo in set(tipos):
-    for item in lista:
-        if type(item) == tipo:
-            print(item)
+# A list comprehension gera uma lista com os tipos
+tipos = [type(item) for item in lista]
 
-# print(dicio)
+# percorre um set com cada tipo de dado
+for tipo in set(tipos):
+    lista_tipo = []
+    # percorre a lista com os itens
+    for item in lista:
+        # se o tipo do dado for igual ao tipo da vez
+        if type(item) == tipo:
+            # armazena dentro da lista_tipo
+            lista_tipo.append(item)
+
+    # Fatia o retorno da string para exibir apenas o tipo
+    nome_tipo = str(tipo)[8:][:-2]
+    # Cria uma chave com o tipo e salva a lista com todos dados de tipo iguais
+    dicio[nome_tipo] = lista_tipo
+
+print(dicio)
+
+# [Saída]:
+# {'int': [1, 2], 'str': ['Python', 'C++'], 'float': [1.2, 3.5], 'list': [[1, 2, 3], [4, 5]]}
+
 
 
 
