547e487b0e3d7f1e12ef7a742e08be9fe2e826f9
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-07-23 12:02:15 -0300

    edit main 42

--- 42_SepadadorDeTiposDeDados/main.py
+++ 42_SepadadorDeTiposDeDados/main.py
@@ -2,8 +2,7 @@ lista = [[1, 2, 3], 'Python', 1, 1.2, [4, 5], 2, 'C++', 3.5]
 dicio = {}
 
 for tipo in set([type(item) for item in lista]):
-    lista_tipo = []
-    [lista_tipo.append(item) for item in lista if tipo == type(item)]
+    lista_tipo = [item for item in lista if tipo == type(item)]
     dicio[str(tipo)[8:][:-2]] = lista_tipo
 
 print(dicio)
