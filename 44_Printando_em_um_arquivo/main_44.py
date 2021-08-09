75fb0829b41b7df97ae77b2b24a6b2f612a13892
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-08-09 15:07:31 -0300

    finalizando 44

--- 44_Printando_em_um_arquivo/main_44.py
+++ 44_Printando_em_um_arquivo/main_44.py
@@ -1,3 +1,13 @@
+# Exemplo 1
 print("Esta dica é muito interessante!", file=open("arquivo.txt", "w"))
 
-print("Adicionando mais texto!", file=open("arquivo.txt", "a"))
+# Exemplo 2
+with open("arquivo.txt", "a") as arquivo_with:
+    print("Adicionando mais texto!", file=arquivo_with)
+
+# Exemplo 3
+arquivo = open("arquivo.txt", "a")
+print("Isso é python !", file=arquivo)
+
+
+
