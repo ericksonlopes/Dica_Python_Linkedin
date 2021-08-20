96eb6eb934204175fa0ca5cdcd1c810fd5d8cf48
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-08-20 18:36:36 -0300

    texto

--- __Concluir__/46_Gerador_de_Captcha/main.py
+++ __Concluir__/46_Gerador_de_Captcha/main.py
@@ -1,9 +1,10 @@
 from captcha.image import ImageCaptcha
 
+# criar uma instância com o tamanho da imagem
 image = ImageCaptcha(width=280, height=90)
 
-texto = 'Python 369'
+# texto para inserir na imagem
+txt = "python 369"
 
-data = image.generate(texto)
-
-image.write(texto, "Captcha.png")
+# Escrever a imagem no arquivo e salvar
+image.write(txt, "Captcha.png")
