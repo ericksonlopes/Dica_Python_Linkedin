7be31291c5071b0648ef4512a5a2bb0acc561980
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-12-06 20:19:54 -0300

    add fotos

---
+++ __Concluir__/ip_webcam/main.py
@@ -0,0 +1,18 @@
+import cv2
+
+ipv4_url = 'http://192.168.0.10:8080'
+
+cam = f"{ipv4_url}/video"
+cap = cv2.VideoCapture(cam)
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (600, 600))
+    cv2.imshow("Camera do celular", frame)
+
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
