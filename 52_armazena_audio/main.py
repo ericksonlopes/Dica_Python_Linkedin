bb2b60ed68de56369b517b987ae0fd84b4e56ef9
Erickson-lopes-dev <ofc.erickson@gmail.com>
2022-01-05 08:01:21 -0300

    52

---
+++ 52_armazena_audio/main.py
@@ -0,0 +1,20 @@
+from gtts import gTTS
+from playsound import playsound
+
+# firetorio + Nome do arquivo
+audio = "voz.mp3"
+
+# Linguagem em que o texto devera ser dito
+language = 'pt-br'
+
+# Texto para ser pronunciado no arquivo .mp3
+text = "Sou programador em python"
+
+# criar objeto com as informações passadas
+sp = gTTS(text=text, lang=language, slow=False)
+
+# Salvar arquivo
+sp.save(audio)
+
+# Ouvir arquivo mp3
+playsound(audio)
