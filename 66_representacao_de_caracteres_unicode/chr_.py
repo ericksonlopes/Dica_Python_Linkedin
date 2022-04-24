3f305070ccfc22836cf42283726f941444da8456
Erickson-lopes <ofc.erickson@gmail>
2022-04-24 12:46:58 -0300

    67

--- 66_representacao_de_caracteres_unicode/chr_.py
+++ 66_representacao_de_caracteres_unicode/chr_.py
@@ -2,9 +2,14 @@
 
 # 65 - 91 Alfabeto maiusculo
 [print(chr(num), end=' ') for num in range(65, 91)]
+# A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 
 print()
 # 97 - 123 Alfabeto minusculo
 [print(chr(num), end=' ') for num in range(97, 123)]
+# a b c d e f g h i j k l m n o p q r s t u v w x y z
 
-# A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+print()
+# 48 - 58 Números
+[print(chr(num), end=' ') for num in range(48, 58)]
+# 0 1 2 3 4 5 6 7 8 9
