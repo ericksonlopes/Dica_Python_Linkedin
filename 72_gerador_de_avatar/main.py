1ec97ad2cb319f3b972df8adf831448b10c822e4
Erickson-lopes-dev <ofc.erickson@gmail.com>
2022-04-26 16:59:44 -0300

    72

--- 72_gerador_de_avatar/main.py
+++ 72_gerador_de_avatar/main.py
@@ -1,6 +1,10 @@
 import pagan
 
-nome = "a"
+input = "Python"
 
-img = pagan.Avatar(nome, pagan.SHA384)
+img = pagan.Avatar(input, pagan.SHA384)
 img.show()
+
+
+img.save(path='imgs/', filename=f'{input}.png')
+img.change(str(input), pagan.SHA384)
