d3aa3f60f7424019b5e99d8af64da64046b36b51
Erickson-lopes-dev <ofc.erickson@gmail.com>
2022-05-04 08:04:11 -0300

    58

--- 58_caminho_atual/cwd.py
+++ 58_caminho_atual/cwd.py
@@ -1,7 +1,14 @@
 import os
+import pathlib
 
+# os
 print(os.getcwd())
-
-# or
-
+print(os.path.abspath(os.getcwd()))
 print(os.path.dirname(os.path.realpath(__file__)))
+
+# pathlib
+print(pathlib.Path().absolute())
+print(pathlib.Path(__file__).parent.absolute())
+
+# Todos com a mesma saída
+# C:\Sample\Python
