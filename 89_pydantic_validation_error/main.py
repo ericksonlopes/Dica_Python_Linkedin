2cd1564fe7203e5b4c513aba1494416423ab70f2
Erickson-lopes-dev <ofc.erickson@gmail.com>
2022-11-08 14:46:28 -0300

    89 add

---
+++ 89_pydantic_validation_error/main.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel, ValidationError
+
+
+class User(BaseModel):
+    id: int
+    name = 'John Doe'
+    signup_ts: Optional[datetime] = None
+    friends: List[int] = []
+
+
+try:
+    external_data = {
+        'id': 1,
+        'signup_ts': 'broken',
+        'friends': [1, 2, 3],
+    }
+
+    User(**external_data)
+
+    user = User(**external_data)
+except ValidationError as e:
+    print(e.json(indent=4))
+    print(e.errors())
